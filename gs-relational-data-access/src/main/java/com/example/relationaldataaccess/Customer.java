97296f20165f8a85860a0810911ef86223d71360
v25bh145 <1368179767@qq.com>
2020-12-02 16:28:08 +0800

    relational-data-access

---
+++ gs-relational-data-access/src/main/java/com/example/relationaldataaccess/Customer.java
@@ -0,0 +1,43 @@
+package com.example.relationaldataaccess;
+
+//在应用程序级表示一个数据(用对象，getter&setter表示，并且覆盖toString方法
+public class Customer {
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "[id: " + id + ", firstName: " + firstName + ", lastName: " + lastName + "]";
+    }
+
+}
